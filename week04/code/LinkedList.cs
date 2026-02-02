3e1c99ae3ff7991e2efacebac4ea85194b4ea24f
yuran-dev <yuran.nhangumbe@gmail.com>
2026-02-02 11:42:39 +0200

    commit

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -201,8 +201,13 @@ public class LinkedList : IEnumerable<int>
     /// </summary>
     public IEnumerable Reverse()
     {
-        // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+         Node? curr = _tail;
+
+    while (curr is not null)
+    {
+        yield return curr.Data;
+        curr = curr.Prev;
+    }
     }
 
     public override string ToString()
