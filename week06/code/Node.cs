6b0201fafd74510b424cd3768142abfc68729c15
yuran-dev <yuran.nhangumbe@gmail.com>
2026-02-18 16:36:58 +0200

    commit

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -6,40 +6,53 @@ public class Node
 
     public Node(int data)
     {
-        this.Data = data;
+        this.Data = data;   
     }
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
-        if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
+        if (value == Data)
+            {
+                // Value already exists, do not insert duplicate
+                return;
+            }
+            else if (value < Data)
+            {
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
             else
-                Left.Insert(value);
-        }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
+            {
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+    {
+        return true;
+    }
+    else if (value < Data)
+    {
+        return Left != null && Left.Contains(value);
+    }
+    else
+    {
+        return Right != null && Right.Contains(value);
+    }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+    int rightHeight = Right?.GetHeight() ?? 0;
+
+    return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -48,6 +48,14 @@ public static class Trees
     /// <param name="bst">the BinarySearchTree in which to insert the values</param>
     private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
     {
-        // TODO Start Problem 5
+        if (first > last)
+        return;
+
+    int middle = first + (last - first) / 2;
+
+    bst.Insert(sortedNumbers[middle]);
+
+    InsertMiddle(sortedNumbers, first, middle - 1, bst);
+    InsertMiddle(sortedNumbers, middle + 1, last, bst); 
     }
 }
